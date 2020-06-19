44dd108508cd3d848363aaa7d977d9fd61eb672b
F-Wer <43503689+F-Wer@users.noreply.github.com>
2020-06-19 09:51:16 +0200

    Comments

--- main.py
+++ main.py
@@ -8,22 +8,25 @@ from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 from selenium import webdriver
-from zipfile import *
+
 from secret import *
+
 '''
 There has to be a file called secret.py with following arg:
 passwort_g='' 
-receiver_email=''
+recipent='' ['test@mail.com','test2@mail.com ... ] You can give this variable n parameters
 sender_email = ""
 username=''
 password=''
 '''
-#Scraping the current grades for the DHBW Ravensburg
 
+
+# Scraping the current grades for the DHBW Ravensburg
 class scrape_Grades():
     def Scrape(self):
-        #Open chrome with a few options so that it's harder for sites to detect a bot
+        # Open chrome with a few options so that it's harder for sites to detect a bot
         options = webdriver.ChromeOptions()
         options.add_argument('--headless')
         options.add_argument("--disable-blink-features=AutomationControlled")
@@ -32,7 +35,8 @@ class scrape_Grades():
         driver = webdriver.Chrome(options=options, executable_path=r'D:\Python\webdriver\chromedriver.exe')
         driver.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browserClientA"}})
         print('Open Chrome')
-        driver.get("https://dualis.dhbw.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000324,-Awelcome")
+        driver.get(
+            "https://dualis.dhbw.de/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=EXTERNALPAGES&ARGUMENTS=-N000000000000001,-N000324,-Awelcome")
         driver.find_element_by_xpath('//*[@id="field_user"]').send_keys(username)
         time.sleep(2)
         driver.find_element_by_xpath('//*[@id="field_pass"]').send_keys(password)
@@ -46,7 +50,7 @@ class scrape_Grades():
         element_png = element.screenshot_as_png
         driver.close()
         print('Close Chrome')
-        #Saving the scraped grades as a picture and as plain text
+        # Saving the scraped grades as a picture and as plain text
         with open("D:/Noten.png", "wb") as file:
             file.write(element_png)
             file.close()
@@ -56,7 +60,7 @@ class scrape_Grades():
         print('Files saved')
 
     def Send_Mail(self):
-        #Copy the current script; I have to save the file as a txt file bc. some email providers don't like executables
+        # Copy the current script; I have to save the file as a txt file bc. some email providers don't like executables
         shutil.copy(__file__, 'D:/main.txt')
         port = 465  # For SSL
         # Create a secure SSL context
@@ -64,13 +68,14 @@ class scrape_Grades():
         # reading the file in binary mode. Because it is saved as a UTF-8 file and there is a error, if you try to convert it to ASCII
         print('Creating E-Mail')
         msg = MIMEMultipart()
-        msg.attach(MIMEText('Dies ist eine automatisch generierte E-Mail für die Abfrage und Speicherung der Noten der DHBW Ravensburg!'))
+        msg.attach(MIMEText(
+            'Dies ist eine automatisch generierte E-Mail für die Abfrage und Speicherung der Noten der DHBW Ravensburg!'))
         msg['Subject'] = 'Dualis'
         msg['From'] = sender_email
-        msg['To'] = ", ".join(receiver_email) #Therefore you can achieve mutliple recipents
-        #path = "D:/"
+        msg['To'] = ", ".join(recipent)  # Therefore you can achieve mutliple recipents
+        # path = "D:/"
         file = ('D:/main.txt', 'D:/Noten.txt', 'D:/Noten.png')
-        #This is in my opinion a cleaner option to send multiple files, but some programs (e.g. 7zip) can't open this zip file
+        # This is in my opinion a cleaner option to send multiple files, but some programs (e.g. 7zip) can't open this zip file
         '''zip = ZipFile("Attachment.zip",mode="w") #Save all files as a zip bc some email providers don't like executables
         for files in file:
             zip.write(path + files, files)
@@ -92,10 +97,8 @@ class scrape_Grades():
         with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
             server.login(sender_email, passwort_g)
             print("Login for E-Mail")
-            server.sendmail(sender_email, receiver_email, msg.as_bytes())
+            server.sendmail(sender_email, recipent, msg.as_bytes())
             print("Email send")
-
-
 
 
 if __name__ == "__main__":
